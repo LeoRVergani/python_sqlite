901527be81bcd21f28e6850dadb5161b7c8209d7
leorvergani <vergani@outlook.com.br>
2024-04-18 23:29:30 -0300

    Tabela corrigida

--- treinosql.py
+++ treinosql.py
@@ -6,17 +6,20 @@ con = sqlite3.connect("cinema.db")
 cur = con.cursor()
 
 # Criar uma tabela
-cur.execute("CREATE TABLE filme(titulo, ano, duracao)")
+# cur.execute("CREATE TABLE filme (titulo text, ano integer, duracao integer)")
 
 # Verificar se tabela foi criada
-res = cur.execute("SELECT name FROM sqlite_master")
-res.fetchone()
+# res = cur.execute("SELECT name FROM sqlite_master")
+# res.fetchone()
 
 # Inserir linhas na tabela
 cur.execute("""
             INSERT INTO filme VALUES
-                ('O Senhor dos Anéis: A Sociedade do Anel', 2001, 178)
+                ('O Senhor dos Anéis: A Sociedade do Anel', 2001, 178),
             ('Conan, o Bárbaro', 1982, 129)
 """)
 con.commit()
+
+cur.execute("SELECT * FROM filme")
+print(cur.fetchall())
 
